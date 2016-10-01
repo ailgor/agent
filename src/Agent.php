8f18c11bc3ff2c92c666e8ca0965842b64bc0657
Diego <diegorys@gmail.com>
2016-10-02 01:19:19 +0200

    First commit.

---
+++ src/Agent.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ailgor\Agent;
+
+class Agent
+{
+	private $sensors;
+	private $actuators;
+	private $memory;
+
+	public function sense()
+	{
+
+	}
+
+	public function map()
+	{
+
+	}
+
+	public function act()
+	{
+		
+	}
+}
---
+++ tests/Agent.php
@@ -0,0 +1,14 @@
+<?php
+
+use Ailgor\Agent\Agent;
+ 
+class AgentTest extends PHPUnit_Framework_TestCase
+{
+ 
+  public function testHello()
+  {
+  	$a = new Agent();
+	$this->assertTrue(true);
+  }
+ 
+}
